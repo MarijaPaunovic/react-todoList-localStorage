b27d454956d918da247932cc10fc1e0711c2237d
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-02-13 13:18:25 +0100

    adding styling, list, alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import List from './components/List.js';
+import Alert from './components/Alert.js';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
   return (
     <div className="App">
-
+      <h3>Todo List using localStorage</h3>
     </div>
   );
 }
